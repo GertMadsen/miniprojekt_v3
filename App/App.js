e6b0db5ded61e45cadae33e0b30db96886b95b87
GertMadsen <cph-gm50@cphbusiness.dk>
2018-11-22 13:24:28 +0100

    small fixes

--- App/App.js
+++ App/App.js
@@ -1,12 +1,31 @@
-import React, { Component } from 'react';
-import { Platform, Text, View, StyleSheet } from 'react-native';
+import React from 'react';
+import { Platform, Button, View, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
 import { Constants, Location, Permissions, MapView } from 'expo';
-const URL = "http://aaceda86.ngrok.io";
 
-export default class App extends Component {
+const serverURL = "http://46.101.214.160"
+
+class HomeScreen extends React.Component {
+  
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: 'Welcome',
+      headerRight: (
+        <Button
+        onPress={() => {
+              /* 1. Navigate to the Details route with params */
+              navigation.navigate('Login');
+            }}
+          title="Login"
+        />
+      ),
+    };
+  };
+
   state = {
     location: null,
     errorMessage: null,
+    isLoading: true
   };
 
   async componentDidMount() {
@@ -17,8 +36,6 @@ export default class App extends Component {
     } else {
       this._getLocationAsync();
     }
-
-
   }
 
   _getLocationAsync = async () => {
@@ -28,51 +45,283 @@ export default class App extends Component {
         errorMessage: 'Permission to access location was denied',
       });
     }
-
     let location = await Location.getCurrentPositionAsync({});
     const { latitude, longitude } = location.coords;
-    //  const gameArea = await fetch(URL+"/geoapi/allowedarea").then(res => res.json())   
-    //  const pos = { coordinates: [longitude, latitude] }
-    //  const opt = {method: "POST", 
-    //               body: JSON.stringify(pos),
-    //               headers: new Headers({
-    //                 "Content-Type": 'application/json'
-    //                 })
-    //               }
-    //  const response = await fetch(URL+"/geoapi", opt ).then(res => res.json());
-    const address = await Location.reverseGeocodeAsync({ latitude, longitude })
-    //  this.setState({gameArea, response, latitude, longitude, address})
-    this.setState({ latitude, longitude, address })
+    this.setState({ latitude, longitude, isLoading: false })
+
   };
 
   render() {
+    return (
+      
+      this.state.isLoading ? <Text>Loading..</Text> :
+      <View style={styles.containerStyle}>
+        <MapView
+          style={styles.mapStyle}
+          initialRegion={{
+            latitude: this.state.latitude,
+            longitude: this.state.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}       >
+
+          <MapView.Marker
+            coordinate={{ longitude: this.state.longitude, latitude: this.state.latitude }}
+            title={"You are here"}
+            description={"Latitude: "+this.state.latitude.toFixed(7)+", Longitude: "+this.state.longitude.toFixed(7)}
+          />
+
+        </MapView>
+        
+        </View>
+    );
+  }
+}
+
+class LoginScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: 'Login',
+    };
+  };
+  state = {
+    user: '',
+    password: '',
+    distance: ''
+ }
+ 
+ handleUser = (text) => {
+    this.setState({ user: text })
+ }
+ handlePassword = (text) => {
+    this.setState({ password: text })
+ }
+ handleDistance = (text) => {
+  this.setState({ distance: text })
+ }
+
+  render() {
+    const { navigation } = this.props;
 
     return (
+      <View style={{ flex: 1, alignItems: 'center' }}>
+        <Text style = {styles.text}>Login to view friends nearby:</Text>
+      <TextInput style = {styles.input}
+         underlineColorAndroid = "transparent"
+         placeholder = " Username"
+         placeholderTextColor = "#9a73ef"
+         autoCapitalize = "none"
+         onChangeText = {this.handleUser}/>
+      
+      <TextInput style = {styles.input}
+         underlineColorAndroid = "transparent"
+         placeholder = " Password"
+         placeholderTextColor = "#9a73ef"
+         autoCapitalize = "none"
+         onChangeText = {this.handlePassword}/>
+      
+      <TextInput style = {styles.input}
+         underlineColorAndroid = "transparent"
+         placeholder = " Distance to search"
+         placeholderTextColor = "#9a73ef"
+         autoCapitalize = "none"
+         onChangeText = {this.handleDistance}/>
+         
+      <TouchableOpacity
+         style = {styles.submitButton}
+         onPress = {
+            () => {
+              navigation.navigate('Friends', {
+                user: this.state.user, password: this.state.password, distance: this.state.distance
+              })
+            }
+         }>
+         <Text style = {styles.submitButtonText}> Submit </Text>
+      </TouchableOpacity>
+
+      </View>
+    );
+  }
+}
+
+class FriendsScreen extends React.Component {
+  state = {
+    location: null,
+    errorMessage: null,
+    isLoading: true,
+    friends: [],
+  };
+
+  async componentDidMount() {
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+      });
+    } else {
+      this._getLocationAsync();
+    }
+    const { navigation } = this.props;
+    this.setState( {user: navigation.getParam('user'), 
+                    password: navigation.getParam('password'),
+                    distance: navigation.getParam('distance'),
+    });
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+    let location = await Location.getCurrentPositionAsync({});
+    const { latitude, longitude } = location.coords;
+    this.setState({ latitude, longitude, isLoading: false })
+    let userToSend = {
+      username: this.state.user,
+      password: this.state.password,
+      longitude: this.state.longitude,
+      latitude: this.state.latitude,
+      distance: Number(this.state.distance),
+    }  
+    const { navigation } = this.props;
+     const opt = {method: "POST", 
+                  body: JSON.stringify(userToSend),
+                  headers: new Headers({
+                    "Content-Type": 'application/json'
+                    })
+                  }
+     const response = await fetch(serverURL+"/api/login", opt ).then(res => res.json());
+     if (response.status == 403) {
+      navigation.navigate('Denied');
+     }             
+     this.setState({friends: response.friends})
+    
+  };
+
+
+
+  render() {
+
+    const { navigation } = this.props;
+    return (
+      this.state.isLoading ? <Text>Loading..</Text> :
+      <View style={styles.containerStyle}>
         <MapView
-          style={{ flex: 1 }}
+          style={styles.mapStyle}
           initialRegion={{
             latitude: this.state.latitude,
             longitude: this.state.longitude,
             latitudeDelta: 0.0922,
             longitudeDelta: 0.0421,
           }}
-        />
+        >
+
+          <MapView.Marker
+            coordinate={{ longitude: this.state.longitude, latitude: this.state.latitude }}
+            title={"You"}
+            description={"Latitude: "+this.state.latitude.toFixed(7)+", Longitude: "+this.state.longitude.toFixed(7)}
+          />
+
+           <MapView.Marker
+            coordinate={{ longitude: this.state.longitude, latitude: friend_lat }}
+            title={"Nearby Friend"}
+            description={"Latitude: "+friend_lat.toFixed(7)+", Longitude: "+this.state.longitude.toFixed(7)}
+            pinColor='green'
+          />
+
+        </MapView>        
+        </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-  },
-  paragraph: {
-    margin: 24,
-    fontSize: 18,
-    textAlign: 'center',
-  },
-});
+class DeniedScreen extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Failed to Login!</Text>
+        <Text>Wrong username or Password.</Text>
+        <Text>Please try again.!</Text>
+        <Button
+          title="Back to Home"
+          onPress={() => this.props.navigation.navigate('Home')}
+        />
+      </View>
+    );
+  }
+}
 
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Login: LoginScreen,
+    Friends: FriendsScreen,
+    Denied: DeniedScreen,
+  },
+  {
+    initialRouteName: 'Home',
+    /* The header config from HomeScreen is now here */
+    defaultNavigationOptions: {
+      headerStyle: {
+        backgroundColor: '#f4511e',
+      },
+      headerTintColor: '#fff',
+      headerTitleStyle: {
+        fontWeight: 'bold',
+      },
+    },
+  }
+);
+
+
+const styles = {
+	containerStyle: {
+		flex:1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: 'lightblue'
+	},
+
+	mapStyle: {
+		left: 0,
+		right: 0,
+		top: 0,
+		bottom: 0,
+		position: 'absolute'
+  },
+  
+ container: {
+    paddingTop: 23
+ },
+ text: {
+   margin: 5,
+   height: 20,
+},
+ input: {
+    margin: 5,
+    height: 40,
+    width: 200,
+    borderWidth: 1
+ },
+ submitButton: {
+    backgroundColor: '#7a42f4',
+    padding: 10,
+    margin: 5,
+    height: 40,
+ },
+ submitButtonText:{
+    color: 'white'
+ }
+
+}
+
+
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
