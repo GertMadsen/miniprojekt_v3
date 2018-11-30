5b07842671a7fa8531e60726a75b85257f9c1c9e
GertMadsen <cph-gm50@cphbusiness.dk>
2018-11-30 12:11:15 +0100

    graphQL start added

--- Server/app.js
+++ Server/app.js
@@ -9,6 +9,10 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
+var graphqlHTTP = require('express-graphql');
+var { schema } = require('./graphql_data/schema');
+
+
 var app = express();
 
 // view engine setup
@@ -25,6 +29,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  graphiql: true,
+}));
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
